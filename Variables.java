e3d89d644010f177db20b29a662f8243e53e2f99
tibera79 <150521260+tibera79@users.noreply.github.com>
2023-12-02 11:56:01 +0100

    Add files via upload

---
+++ Variables.java
@@ -0,0 +1,16 @@
+package dossier2_Variables;
+
+public class Variables {
+    public static void main(String[] args) {
+        int age=12; //déclaration et affectation de la valeur
+        System.out.println("Vous avez " +age+ " ans"); 
+        final int AGE_MAX=42;
+        System.out.println("l'age max est " + AGE_MAX + "ans");
+
+        
+
+
+
+    }
+    
+}
